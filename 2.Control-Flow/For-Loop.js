a700e119f567d80209f259405da947f226a5abb1
Anirudh-rao <anirudh131299@gmail.com>
2022-11-08 00:03:10 +0530

    for-loops in Javascript

---
+++ 2.Control-Flow/For-Loop.js
@@ -0,0 +1,16 @@
+/*
+In programming, loops are used to repeat a block of code.
+*/
+
+const n = 5;
+
+//Looping from i=1 to 5
+for(let i=1; i<=n ;i++){
+    console.log("I Love Javascript");
+}
+
+// looping from i = 1 to 5
+// in each iteration, i is increased by 1
+for (let i = 1; i <= n; i++) {
+    console.log(i);     // printing the value of i
+}
