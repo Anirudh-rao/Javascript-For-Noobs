248d43dd142b18c4445f3faa1d49feb4c20ee529
Anirudh-rao <anirudh131299@gmail.com>
2022-11-12 23:17:35 +0530

    Arrays in Javascript

---
+++ 5.JS-Types/Array.js
@@ -0,0 +1,32 @@
+const words = ['hello','world','welcome'];
+
+console.log(words)
+
+//Creation of the Array
+const array2 = new Array("eat", "sleep");
+
+// empty array
+const myList = [ ];
+console.log(myList)
+
+// array of numbers
+const numberArray = [ 2, 4, 6, 8];
+console.log(numberArray)
+
+// array of strings
+const stringArray = [ 'eat', 'work', 'sleep'];
+console.log(stringArray)
+
+// array with mixed data types
+const newData = ['work', 'exercise', 1, true];
+console.log(newData)
+
+const myArray = ['h', 'e', 'l', 'l', 'o'];
+console.log(myArray)
+
+
+// first element
+console.log(myArray[0]);  // "h"
+
+// second element
+console.log(myArray[1]); // "e"
---
+++ 5.JS-Types/ArrayProps.js
@@ -0,0 +1,61 @@
+//Some Array Properties
+
+let dailyActivities = ['eat', 'sleep'];
+
+// add an element at the end
+dailyActivities.push('exercise');
+
+console.log(dailyActivities); //  ['eat', 'sleep', 'exercise']
+
+//add an element at the start
+dailyActivities.unshift('work');
+
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
+// this will add the new element 'exercise' at the 2 index
+dailyActivities[2] = 'exercise';
+
+console.log(dailyActivities); // ['eat', 'sleep', 'exercise']
+
+
+// this will add the new element 'exercise' at the 3 index
+dailyActivities[3] = 'exercise';
+
+console.log(dailyActivities); // ["eat", "sleep", undefined, "exercise"]
+
+// remove the last element
+dailyActivities.pop();
+console.log(dailyActivities); // ['work', 'eat', 'sleep']
+
+// remove the last element from ['work', 'eat', 'sleep']
+const removedElement = dailyActivities.pop();
+
+//get removed element
+console.log(removedElement); // 'sleep'
+console.log(dailyActivities);  // ['work', 'eat']
+
+// remove the first element
+dailyActivities.shift();
+
+console.log(dailyActivities); // ['eat', 'sleep']
+
+// this gives the total number of elements in an array
+console.log(dailyActivities.length); // 2
+
+
+// sorting elements in the alphabetical order
+dailyActivities.sort();
+console.log(dailyActivities); // ['exercise', 'sleep', 'work']
+
+//finding the index position of string
+const position = dailyActivities.indexOf('work');
+console.log(position); // 2
+
+// slicing the array elements
+const newDailyActivities = dailyActivities.slice(1);
+console.log(newDailyActivities); // [ 'sleep', 'work']
+
+// concatenating two arrays
+let newRoutine = ['eat'];
+const routine = dailyActivities.concat(newRoutine);
+console.log(routine); // ["exercise", "sleep", "work", "eat"]
